1a13af7f9fad545702cc3091e13c1289b904ad22
shrishti.shrishti <shrishti.shrishti@ibm.com>
2023-11-03 16:18:38 +0530

    accordion margin remove

--- src/components/Accordian/index.js
+++ src/components/Accordian/index.js
@@ -4,6 +4,8 @@ import AccordionDetails from "@mui/material/AccordionDetails";
 import AccordionSummary from "@mui/material/AccordionSummary";
 import Typography from "@mui/material/Typography";
 import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import { withStyles, createStyles } from "@mui/styles";
+import "./styles.css";
 
 export default function AccordionComp({ name, content }) {
   const [expanded, setExpanded] = React.useState(false);
@@ -13,7 +15,7 @@ export default function AccordionComp({ name, content }) {
   };
 
   return (
-    <div>
+    <div className="Accordian-custom-styles">
       <Accordion
         expanded={expanded === "panel1"}
         onChange={handleChange("panel1")}
@@ -25,10 +27,17 @@ export default function AccordionComp({ name, content }) {
         }}
       >
         <AccordionSummary
-          expandIcon={<ExpandMoreIcon style={{ color: "#1A2644", fontSize:'30' }} />}
+          expandIcon={
+            <ExpandMoreIcon style={{ color: "#1A2644", fontSize: "30" }} />
+          }
           aria-controls="panel1bh-content"
           id="panel1bh-header"
           sx={{ padding: "0" }}
+          style={{
+            // border:'1px solid red',
+            minHeight: "30px",
+            // margin:'0'
+          }}
         >
           <Typography
             sx={{ flexShrink: 0, color: "#1A2644", fontWeight: "600" }}
@@ -36,7 +45,7 @@ export default function AccordionComp({ name, content }) {
             {name}
           </Typography>
         </AccordionSummary>
-        <AccordionDetails sx={{ paddingLeft: "0" }}>
+        <AccordionDetails sx={{ paddingLeft: "0", paddingTop: "0" }}>
           <Typography
             style={{
               whiteSpace: "pre",
