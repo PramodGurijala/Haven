090367dd1dcd93dd4424eb01f3139ef032bb47d6
Manish Singh <Manish.Singh13@ibm.com>
2023-11-06 16:47:10 +0530

    first commit

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -2,15 +2,18 @@ import React, { useEffect } from "react";
 import "./styles.css";
 import { useNavigate } from "react-router-dom";
 import HomeComp from "../../components/homeComp";
+import SocailMediaComp from "../../components/socialMedia";
+import { Grid } from "@mui/material";
 
 
 export default function HomeScreen() {
   const navigate = useNavigate();
- 
+
 
   return (
     <div>
-      <HomeComp/>
+        <HomeComp />
+        <SocailMediaComp />
     </div>
   );
 }
