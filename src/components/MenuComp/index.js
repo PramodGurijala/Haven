7c55250768286d90b77bf5e7f30c5f7d89d38ddc
CHADALA MOUNICA <CHADALA.Mounica@ibm.com>
2023-11-07 17:53:09 +0530

    Installed Hamburger menu

--- src/components/MenuComp/index.js
+++ src/components/MenuComp/index.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import "./styles.css";
 import { useNavigate } from "react-router-dom";
 import smartupsLogo from "../../assets/smartupsLogo.png";
@@ -6,7 +6,7 @@ import { slide as Menu } from 'react-burger-menu';
 
 
 export default function MenuComp() {
-    //   const [isMenuOpen, setIsMenuOpen] = useState(false);
+      const [isMenuOpen, setIsMenuOpen] = useState(false);
     const navigate = useNavigate();
 
     return (
@@ -16,7 +16,11 @@ export default function MenuComp() {
             <img src={smartupsLogo} alt="SmartUps Logo" />
           </div>
           {/* <div className="topLinks"> */}
-          <Menu>
+          <div style = {{width:'15px'}}>
+          <Menu
+          isOpen = {true} 
+          // onClose={() =>!isMenuOpen}
+          >
           <a id="About Us" className="menu-item">About Us</a>
         <a id="Resources" className="menu-item" >Resources</a>
         <a id="News" className="menu-item" >News</a>
@@ -33,6 +37,7 @@ export default function MenuComp() {
               Sign up
             </a> */}
             </Menu>
+            </div>
         </div>
         </div>
         );
