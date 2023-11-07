62454c31b7c793a8eebebc6dbc63bb694228cfc2
shrishti.shrishti <shrishti.shrishti@ibm.com>
2023-11-07 23:12:21 +0530

    hamburger menu in mobile/tablets

--- src/components/MenuComp/index.js
+++ src/components/MenuComp/index.js
@@ -2,43 +2,109 @@ import React, { useEffect, useState } from "react";
 import "./styles.css";
 import { useNavigate } from "react-router-dom";
 import smartupsLogo from "../../assets/smartupsLogo.png";
-import { slide as Menu } from 'react-burger-menu';
-
+import { slide as Menu } from "react-burger-menu";
+import MenuIcon from "@mui/icons-material/Menu";
+import ArrowBackIosIcon from "@mui/icons-material/ArrowBackIos";
+import { Typography } from "@mui/material";
+import { ScreenSize } from "../../ScreenSize";
 
 export default function MenuComp() {
-      const [isMenuOpen, setIsMenuOpen] = useState(false);
-    const navigate = useNavigate();
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const navigate = useNavigate();
+  const { ScreenWidth } = ScreenSize();
+  return (
+   
+      <div className="topBar">
+        <div className="logo">
+          <img src={smartupsLogo} alt="SmartUps Logo" />
+        </div>
 
-    return (
-        <div>
-        <div className="topBar">
-          <div className="logo">
-            <img src={smartupsLogo} alt="SmartUps Logo" />
-          </div>
-          {/* <div className="topLinks"> */}
-          <div style = {{width:'15px'}}>
-          <Menu
-          isOpen = {true} 
-          // onClose={() =>!isMenuOpen}
-          >
-          <a id="About Us" className="menu-item">About Us</a>
-        <a id="Resources" className="menu-item" >Resources</a>
-        <a id="News" className="menu-item" >News</a>
-        <a id="Products" className="menu-item" >Products</a>
-        <a id="Pricing" className="menu-item" >Pricing</a>
-        <a id="Contact Us" className="menu-item" >Contact Us</a>
-        <a id="Log In" className="menu-item" style={{ textDecoration: "underline" }}>Log In</a>
-        <a id="Sign Up" className="menu-item" >Sign Up</a>
+        {ScreenWidth < 600 ? (
+          <>
+            <Menu
+              customBurgerIcon={
+                <MenuIcon style={{ color: "red", fontSize: "30" }} />
+              }
+              customCrossIcon={<ArrowBackIosIcon style={{ color: "red" }} />}
+              right
+              burgerBarClassName={"burgerBarClassName"}
+              itemListClassName={"menu-list"}
+              menuClassName={"menu-class"}
+              isOpen={false}
+              crossButtonClassName={"menu-crossButtonClassName"}
+              crossClassName={"menu-crossClassName"}
+              overlayClassName={"menu-overlay"}
+              burgerButtonClassName={"menu-burgerButtonClassName"}
+              // noOverlay
+              // isOpen={true}
+              // onClose={() =>!isMenuOpen}
+            >
+              <div
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  width: "100%",
+                  // border: "5px solid blue",
+                }}
+              >
+                <div
+                  style={{
+                    display: "flex",
+                    height: "60px",
+                    alignItems: "center",
+                    paddingLeft: "60px",
+                    color: "#1A264480",
+                    fontWeight: "500",
+                  }}
+                >
+                  Menu
+                </div>
+                <div
+                  style={{
+                    borderBottom: "2px solid #1A264433",
+                    borderTop: "2px solid #1A264433",
+                    margin: "0px 25px",
+                    padding: "20px 0px",
+                  }}
+                >
+                  Login
+                </div>
+                <div
+                  style={{
+                    display: "flex",
+                    flexDirection: "column",
+                    padding: "20px 25px",
+                    gap: "25px",
+                  }}
+                >
+                  <a id="Pricing" className="menu-item">
+                    Pricing
+                  </a>
 
-            {/* <a href="#" style={{ textDecoration: "underline" }}>
-              Log in
-            </a>
-            <a href="#" className="signupButton">
-              Sign up
-            </a> */}
+                  <a id="About Us" className="menu-item">
+                    About Us
+                  </a>
+                  <a id="Resources" className="menu-item">
+                    Resources
+                  </a>
+                  <a id="News" className="menu-item">
+                    News
+                  </a>
+                  <a id="Products" className="menu-item">
+                    Products
+                  </a>
+
+                  <a id="Contact Us" className="menu-item">
+                    Contact Us
+                  </a>
+                </div>
+              </div>
             </Menu>
-            </div>
-        </div>
-        </div>
-        );
+          </>
+        ) : (
+          <>browser menu</>
+        )}
+      </div>
+   
+  );
 }
