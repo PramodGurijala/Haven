21f81429eccbfc780f9b0e6b17940222fb0ff658
Manish Singh <Manish.Singh13@ibm.com>
2023-11-08 16:32:20 +0530

    sty

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -4,6 +4,8 @@ import { useNavigate } from "react-router-dom";
 import HomeComp from "../../components/homeComp";
 import SocailMediaComp from "../../components/socialMedia";
 import LaunchFooter from "../../components/LaunchFooter";
+import LaunchHeader from "../../components/LaunchHeader";
+import images from "../../config/images";
 import { Grid } from "@mui/material";
 
 
@@ -13,6 +15,10 @@ export default function HomeScreen() {
 
   return (
     <div>
+        <Grid p={2}>
+        <img alt="" style={{maxWidth:'250px'}} width={'25%'} src={images.smartupsLogo} />
+      </Grid>
+      <LaunchHeader />
         <HomeComp />
         <SocailMediaComp />
         <LaunchFooter/>
