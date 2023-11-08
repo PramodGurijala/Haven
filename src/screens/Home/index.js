27273a5c63b20a9ea42134a8da72ec147f54ab75
shrishti.shrishti <shrishti.shrishti@ibm.com>
2023-11-08 16:12:29 +0530

    home screen header for web

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -5,21 +5,56 @@ import HomeComp from "../../components/homeComp";
 import MenuComp from "../../components/MenuComp";
 import SocailMediaComp from "../../components/socialMedia";
 import LaunchFooter from "../../components/LaunchFooter";
-import LaunchHeader from "../../components/LaunchHeader";
-import images from "../../config/images";
-import { Grid } from "@mui/material";
-
+import { Grid, Typography } from "@mui/material";
+import { ScreenSize } from "../../ScreenSize";
+import { HomeData } from "../../screens/Home/HomeScreenData";
 
 export default function HomeScreen() {
   const navigate = useNavigate();
-
-
+  const { ScreenWidth } = ScreenSize();
   return (
     <div>
-      <MenuComp/>
-      <HomeComp/>
-        <SocailMediaComp />
-        <LaunchFooter/>
+      <Grid p={2}>
+        <Typography>header component</Typography>
+      </Grid>
+      <Grid className="homescreen-topheader">
+        <Typography className="heading-top-homescreen">
+          Welcome to Smartups,
+          <br />
+          your digital business
+          {ScreenWidth < 601 ? <br /> : null}
+          support platform.
+          <br />
+          Come for the features,
+          <br />
+          stay for the support.
+        </Typography>
+      </Grid>
+
+      <div className="mainContainer">
+        {HomeData.map((item, index, key) => {
+          return (
+            <div
+              className={
+                index % 2 === 0 ? "subContainer" : "subContainerReverse"
+              }
+            >
+              <div className="article">
+                <div className="subArticle">
+                  <h2 className="header">{item.title}</h2>
+                  <p className="paragraph">{item.paragraph}</p>
+                  <button className="btn">{item.btnText}</button>
+                </div>
+              </div>
+              <div className="ImgBox">
+                <img src={item.img_url} width="100%" />
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      <SocailMediaComp />
+      <LaunchFooter />
     </div>
   );
 }
