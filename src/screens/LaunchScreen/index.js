008702f7e19a64f47c4fa4650069e2cd881f4056
shrishti.shrishti <shrishti.shrishti@ibm.com>
2023-11-08 15:08:14 +0530

    launch header in launch screen fixed

--- src/screens/LaunchScreen/index.js
+++ src/screens/LaunchScreen/index.js
@@ -5,8 +5,7 @@ import { TextField, Grid, Typography, Checkbox } from "@mui/material";
 import Button from "../../components/Button";
 import { ScreenSize } from "../../ScreenSize";
 import images from "../../config/images";
-import LaunchFooter from '../../components/LaunchFooter';
-import LaunchHeader from "../../components/LaunchHeader";
+import LaunchFooter from "../../components/LaunchFooter";
 import SocailMediaComp from "../../components/socialMedia";
 export default function LaunchScreen() {
   const navigate = useNavigate();
@@ -14,10 +13,60 @@ export default function LaunchScreen() {
   return (
     <Grid className="LaunchScreen">
       <Grid p={2}>
-        <img alt="" style={{maxWidth:'250px'}} width={'25%'} src={images.smartupsLogo} />
+        <Typography>header component</Typography>
       </Grid>
-      <LaunchHeader />
-      <SocailMediaComp/>
+      <Grid className="LaunchHeader">
+        <img
+          alt=""
+          src={
+            isLaptop
+              ? images.bg_dt_launch
+              : isMobile
+              ? images.bg_m_launch
+              : images.bg_tb_launch
+          }
+          style={{
+            width: "100vw",
+            position: "absolute",
+          }}
+        />
+        <Grid
+          className="top_launch"
+          style={{
+            aspectRatio: isLaptop ? 2.26 : isMobile ? 0.93 : 1.68,
+          }}
+        >
+          <Typography className="heading-top-launch">
+            Welcome to Smartups.
+            <br />
+            Your digital business
+            <br />
+            management platform.
+          </Typography>
+          <Typography className="body-top-launch">
+            How will you be using our platform?
+            <br />
+            Select the button that best describes you.
+          </Typography>
+          <Grid className="launch-top-btn">
+            <Grid mx="2%">
+              <Button
+                color="white"
+                onClick={() => navigate("/HomeScreen")}
+                name="Startup"
+              />
+            </Grid>
+            <Grid mx="2%">
+              <Button
+                color="white"
+                // onClick={() => viewCartFunc()}
+                name="Incubator"
+              />
+            </Grid>
+          </Grid>
+        </Grid>
+      </Grid>
+      <SocailMediaComp />
       <LaunchFooter />
     </Grid>
   );
